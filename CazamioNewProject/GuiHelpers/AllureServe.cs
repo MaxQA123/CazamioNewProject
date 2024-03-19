b06c2a25d85110ae27399525ef50d091b3620a79
Максим Перевалов <nnpctonsp@gmail.com>
2024-03-19 11:47:05 +0200

    3.19.2024 11 : 45

---
+++ CazamioNewProject/GuiHelpers/AllureServe.cs
@@ -0,0 +1,24 @@
+﻿using Allure.Commons;
+using NUnit.Allure.Attributes;
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace CazamioNewProject.GuiHelpers
+{
+    public class AllureServe
+    {
+        [Test]
+        [AllureTag("Regression")]
+        [AllureSeverity(SeverityLevel.critical)]
+        [AllureOwner("Perevalov Maksim")]
+        [AllureSuite("WebSite")]
+        [AllureSubSuite("Client")]
+
+
+        public void GoToAllureResults()
+        {
+            AllureConfigFilesHelper.CreateBatFile();
+            Process.Start(Browser.RootPath() + "allure serve.bat");
+        }
+    }
+}
