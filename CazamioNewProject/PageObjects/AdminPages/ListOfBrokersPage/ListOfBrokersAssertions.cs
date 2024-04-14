ff443a6255b428dedd80fad340a955a497c85bd3
Максим Перевалов <nnpctonsp@gmail.com>
2024-04-14 18:43:21 +0300

    14.4.2024 18 : 47

--- CazamioNewProject/PageObjects/AdminPages/ListOfBrokersPage/ListOfBrokersAssertions.cs
+++ CazamioNewProject/PageObjects/AdminPages/ListOfBrokersPage/ListOfBrokersAssertions.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CazamioNewProject.GuiHelpers;
+using NUnit.Allure.Attributes;
+using NUnit.Framework;
 
 namespace CazamioNewProject.PageObjects.AdminPages.ListOfBrokersPage
 {
     public partial class ListOfBrokers
     {
+        [AllureStep("VerifyTitleListOfBrokersPg")]
+        public ListOfBrokers VerifyTitleListOfBrokersPg()
+        {
+            WaitUntil.CustomElementIsVisible(TitleListOfBrokers);
+            Assert.IsTrue(Successfully.IsVisible(TitleListOfBrokers));
+
+            return this;
+        }
     }
 }
--- MarketplaceAdminGuiTest/MarketplaceAdminTests/MarketplaceAdminTests.cs
+++ MarketplaceAdminGuiTest/MarketplaceAdminTests/MarketplaceAdminTests.cs
@@ -20,20 +20,128 @@ namespace MarketplaceAdminGuiTest
         [Author("Maksim", "maxqatesting390@gmail.com")]
         [AllureSuite("MarketplaceAdmin")]
         [AllureSubSuite("LogIn")]
+
         public void LogIn()
         {
+            #region Test
+
             Pages.LogInLandlord
                 .EnterEmailPasswordAsMarketplaceAdmin()
                 .ClickIconShow()
                 .ClickButtonLetsGo();
 
-            string getUserNameCompare = Pages.Sidebar.GetUserNameFromSideBar();
-            string getUserNameRoleCompare = Pages.Sidebar.GetUserNameRoleFromSideBar();
+            string getUserNameCompare = Pages.SidebarLandlord.GetUserNameFromSideBar();
+            string getUserNameRoleCompare = Pages.SidebarLandlord.GetUserNameRoleFromSideBar();
 
-            Pages.Sidebar
+            Pages.SidebarLandlord
                 .VerifyMarketplaceAdminUserNameAndRole(getUserNameCompare, getUserNameRoleCompare);
 
             WaitUntil.WaitSomeInterval(5000);
+            #endregion
+        }
+
+        [Test]
+        [AllureTag("Regression")]
+        [AllureOwner("Maksim Perevalov")]
+        [AllureSeverity(SeverityLevel.critical)]
+        [Retry(2)]
+        [Author("Maksim", "maxqatesting390@gmail.com")]
+        [AllureSuite("MarketplaceAdmin")]
+        [AllureSubSuite("VerifySidebar")]
+
+        public void VerifySidebar()
+        {
+            #region Test
+
+            Pages.LogInLandlord
+                .EnterEmailPasswordAsMarketplaceAdmin()
+                .ClickIconShow()
+                .ClickButtonLetsGo();
+
+            string getUserNameCompare = Pages.SidebarLandlord.GetUserNameFromSideBar();
+            string getUserNameRoleCompare = Pages.SidebarLandlord.GetUserNameRoleFromSideBar();
+
+            Pages.SidebarLandlord
+                .VerifyMarketplaceAdminUserNameAndRole(getUserNameCompare, getUserNameRoleCompare);
+
+            #endregion
+
+            #region Test
+
+            Pages.SidebarLandlord
+                .UploadImageLogoLandlordFirst()
+                .VerifyChangingLogoImageLandlord()
+                .UploadImageLogoLandlordSecond()
+                .VerifyChangingLogoImageLandlord()
+                .UploadImageAvatarUserLandlordFirst()
+                .VerifyChangingAvatarImageLandlord()
+                .UploadImageAvatarUserLandlordSecond()
+                .VerifyChangingAvatarImageLandlord()
+                .ClicklinkRemovePhotoOfLandlord()
+                .VerifyRewmoveAvatarImageLandlord();
+            //    .ClickButtonDashboardSidebar();
+            //Pages.Dashboard
+            //    .VerifyTitleOfDashboardPg();
+            Pages.SidebarLandlord
+                .ClickButtonBuildings();
+            Pages.ListOfBuildings
+                .VerifyTitleListOfBuildings();
+            Pages.SidebarLandlord
+                .ClickButtonApartments();
+            Pages.ListOfApartments
+                .VerifyTitleListOfApartments();
+            Pages.SidebarLandlord
+                .ClickButtonApplications();
+            Pages.ListOfApplications
+                .VerifyTitleListOfApplications();
+            Pages.SidebarLandlord
+                .ClickButtonBrokers();
+            Pages.ListOfBrokers
+                .VerifyTitleListOfBrokersPg();
+            Pages.SidebarLandlord
+                .ClickButtonLeaseSignDocuments();
+            Pages.Templates
+                .VerifyTitleTemplatesPage();
+            Pages.SidebarLandlord
+                .ClickButtonPaymentSettings();
+            Pages.PaymentSettings
+                .VerifyTitlePaymentSettingsPage();
+            Pages.SidebarLandlord
+                .ClickButtonTransactions();
+            Pages.ListOfTransactions
+                .VerifyTitleTransactionsPg();
+            Pages.SidebarLandlord
+               .ClickButtonLeads();
+            Pages.ListOfLeads
+                .VerifyTitleLeads();
+            Pages.SidebarLandlord
+                .ClickButtonCommissions();
+            Pages.ListOfCommissions
+                .VerifyTitleListOfCommissions();
+            Pages.SidebarLandlord
+                .ClickButtonAgents();
+            Pages.ListOfAgents
+                .VerifyTitleAgentsPg();
+            Pages.SidebarLandlord
+                .ClickButtonOwners();
+            Pages.ListOfOwners
+                .VerifyTitleListOfOwnersPg();
+            Pages.SidebarLandlord
+               .ClickButtonMarketplace();
+            Pages.Marketplace
+                .VerifyTitleMarketplacePg();
+            Pages.SidebarLandlord
+                .ClickButtonReports();
+            Pages.Reports
+                .VerifyTitleReportsPage();
+            Pages.AreYouSureLogOutLandlord
+                .MakeLogOut();
+            Pages.LogInLandlord
+                .VerifyTitle();
+
+            WaitUntil.WaitSomeInterval(2000);
+
+            #endregion
         }
     }
 }
