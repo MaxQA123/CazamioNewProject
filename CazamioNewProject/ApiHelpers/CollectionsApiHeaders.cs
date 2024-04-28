a13c1830c5bc399d12029e3568f29e515f8a2fb5
Максим Перевалов <nnpctonsp@gmail.com>
2024-04-28 19:31:25 +0300

    28.4.2024 19 : 29

---
+++ CazamioNewProject/ApiHelpers/CollectionsApiHeaders.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace CazamioNewProject.ApiHelpers
+{
+    public class Headers
+    {
+        public static ICollection<KeyValuePair<string, string>> HeadersTenant()
+
+        {
+            var headersCommon = new List<KeyValuePair<string, string>>();
+
+            headersCommon.Add(new KeyValuePair<string, string>("accept", "application/json, text/plain, */*"));
+            headersCommon.Add(new KeyValuePair<string, string>("accept-encoding", "gzip, deflate, br"));
+            headersCommon.Add(new KeyValuePair<string, string>("Content-Type", "application/json"));
+            headersCommon.Add(new KeyValuePair<string, string>("api-version", "1"));
+            headersCommon.Add(new KeyValuePair<string, string>("marketplace", "https://landlord-demo.casamio-test.lol"));
+
+            return headersCommon;
+        }
+
+        public static ICollection<KeyValuePair<string, string>> HeadersAdmins()
+
+        {
+            var headersCommon = new List<KeyValuePair<string, string>>();
+
+            headersCommon.Add(new KeyValuePair<string, string>("accept", "application/json, text/plain, */*"));
+            headersCommon.Add(new KeyValuePair<string, string>("accept-encoding", "gzip, deflate, br"));
+            headersCommon.Add(new KeyValuePair<string, string>("Content-Type", "application/json"));
+            headersCommon.Add(new KeyValuePair<string, string>("api-version", "1"));
+
+            return headersCommon;
+        }
+    }
+}
---
+++ CazamioNewProject/ApiHelpers/TestDataApi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CazamioNewProject.ApiHelpers
+{
+    public class BaseStartPointsApi
+    {
+        public const string API_HOST_WEBSITE_LANDLORD = "https://api-cazamio-staging.azurewebsites.net";
+        public const string API_HOST_WEBSITE_TENANT = "https://api-cazamio-staging.azurewebsites.net";
+    }
+
+    public class ApiRequestData
+    {
+        public const string DEVICE_FINGERPRINT = "5ab5f20f5788afc5e04f1bd0317c8356";
+        public const bool TRUE = true;
+        public const bool FALSE = false;
+        public const string RETURN_URL = "/";
+    }
+
+    public class ApiTestDataGeneral
+    {
+        public const string BRAND_NAME_MARKETPLACE_ONE = "Beautiful Life";
+    }
+
+    public class ApiTestDataBroker
+    {
+        public const string DEVICE_FINGERPRINT = "5ab5f20f5788afc5e04f1bd0317c8356";
+    }
+
+    public class ApiTestDataTenants
+    {
+        public const string DEVICE_FINGERPRINT = "417d13cf3013cfd714a7c316d0415eca";
+    }
+}
