802cfe2bc8fa3548f0020863e5cdf664e1b761b6
Максим Перевалов <nnpctonsp@gmail.com>
2024-08-28 09:50:53 -0400

    28-8-2024 16-49

--- CazamioNewProject/GuiHelpers/GenerateRandomData.cs
+++ CazamioNewProject/GuiHelpers/GenerateRandomData.cs
@@ -21,6 +21,14 @@ namespace CazamioNewProject.GuiHelpers
                 .Select(s => s[random.Next(s.Length)]).ToArray());
         }
 
+        public static string RandomAlphabet(int size)
+        {
+            Random random = new Random();
+            const string chars = "QWERTYUIOPASDFGHJKLZXCVBNM";
+            return new string(Enumerable.Repeat(chars, size)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
         public static string RandomNumberWithZero(int size)
         {
             Random random = new Random();
