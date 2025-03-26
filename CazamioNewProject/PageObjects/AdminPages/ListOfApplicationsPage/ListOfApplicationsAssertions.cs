d280eff64039a683e28b934161ac223af5e4919b
Максим Перевалов <nnpctonsp@gmail.com>
2025-03-26 14:37:29 -0400

    26-3-2025 20-35

--- CazamioNewProject/PageObjects/AdminPages/ListOfApplicationsPage/ListOfApplicationsAssertions.cs
+++ CazamioNewProject/PageObjects/AdminPages/ListOfApplicationsPage/ListOfApplicationsAssertions.cs
@@ -1,11 +1,6 @@
 ﻿using CazamioNewProject.GuiHelpers;
 using NUnit.Allure.Attributes;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CazamioNewProject.PageObjects.AdminPages.ListOfApplicationsPage
 {
