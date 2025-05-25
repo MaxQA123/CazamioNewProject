df66e8993ed171e85a042f9deee863cbc0071bd0
Максим Перевалов <nnpctonsp@gmail.com>
2025-05-25 03:49:18 -0400

    25-5-2025 10-48

--- CazamioNewProject/Objects/EmailNotificationsForMarketplaceAdmin.cs
+++ CazamioNewProject/Objects/EmailNotificationsForMarketplaceAdmin.cs
@@ -4,36 +4,28 @@ namespace CazamioNewProject.Objects
 {
     public class EmailNotificationsForMarketplaceAdmin
     {
-        public SubjectsBasicInfo SubjectsBasic { get; set; }
+        public SubjectsAndBodiesBasicInfo SubjectsAndBodiesBasic { get; set; }
 
         public static EmailNotificationsForMarketplaceAdmin Generate()
         {
             return new EmailNotificationsForMarketplaceAdmin
             {
-                SubjectsBasic = CreateSubjectsBasicInfo(),
+                SubjectsAndBodiesBasic = CreateSubjectsAndBodiesBasicInfo(),
             };
         }
 
-        public class SubjectsBasicInfo
+        public class SubjectsAndBodiesBasicInfo
         {
-            public string CreateTenantViaGetLinkWithoutAgentSaintJohnsonPlace { get; set; }
-            public string CreateTenantViaGetLinkWithAgentBrokerSaintJohnsonPlace { get; set; }
-            public string CreateTenantViaGetLinkWithoutAgentAlbermaleRd { get; set; }
-            public string CreateTenantViaPlusAppWithAgentWashingtonSquare { get; set; }
-            public string CreateTenantOccupantViaPlusAppWithoutAgentAlbermaleRd { get; set; }
-            public string CreateTenantOccupantViaPlusAppWithoutAgentDeanStreet { get; set; }
+            public string SubjectCreatingNewMarketplaceAdmin { get; set; }
+            public string BodyCreatingNewMarketplaceAdmin { get; set; }
         }
 
-        private static SubjectsBasicInfo CreateSubjectsBasicInfo()
+        private static SubjectsAndBodiesBasicInfo CreateSubjectsAndBodiesBasicInfo()
         {
-            return new SubjectsBasicInfo
+            return new SubjectsAndBodiesBasicInfo
             {
-                CreateTenantViaGetLinkWithoutAgentSaintJohnsonPlace = "Welcome to Noyo Properties NYC! Let's begin your application process for 9998 Saint Johnson Place #1 now!",
-                CreateTenantViaGetLinkWithAgentBrokerSaintJohnsonPlace = "Welcome to Noyo Properties NYC! Agentus Brokerus created you an application for 9998 Saint Johnson Place #1.",
-                CreateTenantViaGetLinkWithoutAgentAlbermaleRd = "Welcome to Noyo Properties NYC! Leoautotest Feabroker created you an application for 9A Albermale Rd #1.",
-                CreateTenantViaPlusAppWithAgentWashingtonSquare = "Welcome to Noyo Properties NYC! Lula AgentQA created you an application for 1 Washington Square #8R.",
-                CreateTenantOccupantViaPlusAppWithoutAgentAlbermaleRd = "Welcome to Noyo Properties NYC! Let's begin your application process for 9A Albermale Rd #1 now!",
-                CreateTenantOccupantViaPlusAppWithoutAgentDeanStreet = "Welcome to Noyo Properties NYC! Let's begin your application process for 12567 Dean Street #1 now!",
+                SubjectCreatingNewMarketplaceAdmin = "Welcome Aboard as a Marketplace Admin at Doorway LLC!",
+                BodyCreatingNewMarketplaceAdmin = "",
             };
         }
     }
